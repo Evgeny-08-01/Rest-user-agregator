98affaf950959361fddd1afb6958de9538895370
Воробьев Евгений <vorobiev_ev@mail.ru>
2026-07-23 13:02:58 +0300

    refactor: main.go architecture and workflows env variables
    MAIN.GO CHANGES:
    -----------------
    - main() now only calls run() and handles fatal errors
    - run() orchestrates the application lifecycle
    - LOG_PATH can be overridden via .env with fallback to Docker/local paths
    - Migration logic split into shouldRollback(), rollbackMigrations(), applyMigrations()
    - initDB() returns error instead of calling logger.Fatal
    - All functions follow SRP
    - Proper error wrapping: %w for returns, %v for logging
    
    WORKFLOW CHANGES:
    ------------------
    - Added global env: block at workflow level
    - Variables: DB_PATH, LOG_LEVEL, ENV, LOG_PATH, SERVER_PORT
    - Available for all jobs (test and publish)
    
    FILES CHANGED:
    ---------------
    - cmd/api/main.go
    - .github/workflows/workflows.yml

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,9 @@
 package main
 
+// ============================================================
+// 1. ИМПОРТЫ 
+// ============================================================
+
 import (
 	"context"
 	"net/http"
@@ -8,6 +12,7 @@ import (
 	"syscall"
 	"time"
     "log"
+	"fmt"
 
 	_ "github.com/Evgeny-08-01/Rest-user-agregator/docs"
 	"github.com/Evgeny-08-01/Rest-user-agregator/internal/database"
@@ -16,68 +21,126 @@ import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/swaggo/http-swagger"
-
 )
-
 // @title Subscription API
 // @version 1.0
 // @SERVER_PORT=8080
 // @BasePath /api
+// ============================================================
+// 2. ТОЧКА ВХОДА
+// ============================================================
 func main() {
+    if err := run(); err != nil {
+        log.Fatalf("Fatal error: %v", err)
+    }
+}
+
+// ============================================================
+// 3. ОСНОВНАЯ ЛОГИКА (СБОРКА И ЗАПУСК)
+// ============================================================
+// Структура проекта
+//  func run() error {
+//  1. .env
+//  2. Логгер
+//  3. БД
+//  4. Миграции
+//  5. Сервер
+//    return nil
+//}
+func run() error {
+	// 1. Загружаем .env
+    loadEnv()
+	// 2. Инициализация моего Логгера
+	initLogger()
+	// 3. Инициализация БД
+	if err := initDB(); err != nil {
+		return fmt.Errorf("DB init: %w", err)
+	}
+defer database.Close()                                // Откладываем закрытие БД до завершения программы
+logger.Info("Database connected successfully")
+
+	// 4. Миграции
+	if err := runMigrations(); err != nil {
+		return fmt.Errorf("migrations: %w", err)
+	}
+
+	// 5. Сервер
+	if err := startServer(); err != nil {
+		return fmt.Errorf("server: %w", err)
+	}
+
+	return nil
+}
+// ============================================================
+// 4. ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ (каждая делает что то одно)
+// ============================================================
+
+// 4.1 Загрузка .env
+func loadEnv() {
 if err := godotenv.Load(".env"); err != nil {
     log.Println("[WARN] .env file not found, using default values")
 } else {
     log.Println("[INFO] .env file loaded successfully")
-    // Выведи значение DB_PATH для проверки
     log.Printf("[INFO] DB_PATH from .env: %s", os.Getenv("DB_PATH"))
 }
+}
 
- // 2. Инициализируем логгер (читаем уровень из .env)
-    logLevel := os.Getenv("LOG_LEVEL")
+// 4.2 Инициализация моего логгера(читаем уровень из .env)
+func initLogger() {            
+logLevel := os.Getenv("LOG_LEVEL")           // читает переменную окружения LOG_LEVEL
+logPath := os.Getenv("LOG_PATH")             // читает переменную окружения LOG_PATH
     if logLevel == "" {
-        logLevel = "info"
-    
-logPath := "/var/log/app/app.log"
-if os.Getenv("ENV") != "docker" {
-    logPath = "./logs/app.log"
-}	
-logger.Init(logPath, logLevel)
-logger.Info("Starting Subscription API server") 
+        logLevel = "info"                    //   default level 
+	}	
 
-	// 2. Получаем путь к БД ИЗ .env
-databasePath := os.Getenv("DB_PATH")
+    if logPath == "" {
+        if os.Getenv("ENV") == "docker" {            
+            logPath = "/var/log/app/app.log"         // путь к файлу с логами для Docker, когда поднимаем Docker-контейнер...docker compose up
+		                                             //  или CI/CD pipeline
+        } else {
+            logPath = "./logs/app.log"               // путь к файлу с логами для локальной разработки ,без Docker... go run main.go
+        }
+    }
+logger.Init(logPath, logLevel)
+logger.Info("Starting Subscription API server")    }
+
+// 4.3 Подключение к БД
+func initDB() error {
+databasePath := os.Getenv("DB_PATH")                                                   // Получаем путь к БД ИЗ .env
 if databasePath == "" {
-    databasePath = "postgres://postgres:mysecret@db:5432/subscriptions?sslmode=disable"
+    databasePath = "postgres://postgres:mysecret@db:5432/subscriptions?sslmode=disable"// если не получили, то ставим default
 	logger.Warn("DB_PATH not set, using default")
 }
-	// 3. Подключаемся к БД
-	err := database.Init(databasePath) // Подключение к БД
+	err := database.Init(databasePath)                                                  // Подключение к БД
 	if err != nil {
 		 logger.Fatal("Failed to connect to database: %v", err) 
 	}
-// Откладываем закрытие БД до завершения программы
-defer database.Close()
-logger.Info("Database connected successfully")
+	 return nil 
+}
 
-// 4. Проверяем на наличие миграций
-if len(os.Args) > 1 && os.Args[1] == "-down" {
-    if err := database.RollbackMigrations(); err != nil {
-        logger.Fatal("Failed to rollback migration: %v", err)
+// 4.4 Миграции
+func runMigrations() error {
+    if shouldRollback() {
+        return rollbackMigrations()
     }
-    logger.Info("Migration rolled back")
-    return
+    return applyMigrations()
 }
 
-// 5. Запускаем миграции
-if err := database.RunMigrations(); err != nil {
-    logger.Warn("Migrations warning (maybe already applied): %v", err)
-}
-	// 6. Роутер (switch для URL)
-	repo := database.NewPostgresRepo()
-    handler := handlers.NewHandler(repo)
-	mux := http.NewServeMux()
+// 4.5 Запуск сервера
+func startServer() error {              
+	repo := database.NewPostgresRepo()      // экземпляр репозитория, содержащий пул соединений и указатель на БД,
+	                                        //  содержит методы работы с БД. NewPostgresRepo-конструктор над PostgresRepo
+											// PostgresRepo- структура и содежит поле: db *sql.DB 
 
-	// 7. CRUDL операции
+    handler := handlers.NewHandler(repo)    // экземпляр хендлера, содержащий экземпляр репозитория repo для работы с БД,
+                                            // содержит методы обработки HTTP-запросов.
+                                            // NewHandler — конструктор, создающий экземпляр Handler.
+                                            // Handler — структура с полем Repo(тип интерфейс) repository.SubscriptionRepository(интерфейс).
+											// !!!! Таким образом  handler содержит методы обработки запросов и подключение к БД
+										
+	mux := http.NewServeMux()               // Создаем роутер-switch для URL
+
+	// CRUDL операции
 	mux.HandleFunc("POST    /api/subscriptions",               handlers.LoggingMiddleware(handler.CreateSubscriptionHandler))
 	mux.HandleFunc("GET     /api/subscriptions/{id}",          handlers.LoggingMiddleware(handler.GetSubscriptionHandler))
 	mux.HandleFunc("PUT     /api/subscriptions/{id}",          handlers.LoggingMiddleware(handler.UpdateSubscriptionHandler))
@@ -85,46 +148,66 @@ if err := database.RunMigrations(); err != nil {
 	mux.HandleFunc("GET     /api/subscriptions",               handlers.LoggingMiddleware(handler.ListSubscriptionsHandler))
 	mux.HandleFunc("GET     /api/subscriptions/total-cost",    handlers.LoggingMiddleware(handler.GetTotalCostHandler))
 	mux.HandleFunc("GET     /swagger/",                        httpSwagger.WrapHandler)
-	// 8. Получаем порт из .env
+
+	//  Получаем порт из .env
 	port := os.Getenv("SERVER_PORT")
 	if port == "" {
 		port = "8080"
 		logger.Warn("SERVER_PORT not set, using default 8080") 
 	}
-   // 9. HTTP сервер с таймаутами 
-    srv := &http.Server{
-        Addr:         ":" + port,
-        Handler:      mux,
-        ReadTimeout:  5  * time.Second,
-        WriteTimeout: 10 * time.Second,
-        IdleTimeout:  15 * time.Second,
-    }
- // 10. Запускаем сервер в горутине
+// Создаем HTTP сервер с таймаутами 
+    srv := &http.Server{                  // указатель на структуру http.Server.... поля структуры:
+
+    Addr:         ":" + port,             // адрес и порт, на котором сервер будет слушать запросы
+    Handler:      mux,                    // роутер, который будет обрабатывать входящие запросы
+    ReadTimeout:  5 * time.Second,        // максимальное время на чтение всего запроса (заголовки + тело) — защита от медленных клиентов
+    WriteTimeout: 10 * time.Second,       // максимальное время на запись ответа — защита от зависших хендлеров
+    IdleTimeout:  15 * time.Second,       // максимальное время жизни keep-alive соединения без новых запросов
+}
+ // Запускаем сервер в горутине
     go func() {
              logger.Info("Server starting on port %s", port) 
-        if err2 := srv.ListenAndServe(); err2 != nil && err2 != http.ErrServerClosed {
+        if err2 := srv.ListenAndServe(); err2 != nil && err2 != http.ErrServerClosed { // Обработка ошибок сервера
            logger.Error("Server failed: %v", err2)
-			os.Exit(1) // Завершаем программу с кодом 1
+			os.Exit(1)                                                                 // Завершаем программу по аварии с кодом 1
         }
     }()
 
-// 11. Graceful shutdown (ожидание сигнала)
-    quit := make(chan os.Signal, 1)
-    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-    <-quit
+//  Graceful shutdown (ожидание сигнала на отключение)
+    quit := make(chan os.Signal, 1)                            // Создаем канал для ожидания сигналов
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)       // Наблюдаем за сигналами SIGINT и SIGTERM
+    <-quit                                                     // Блокируем до получения сигнала
 
 logger.Info("Shutting down server...")
-// 12. Контекст с таймаутом на завершение (5 секунд)
-    ctx, cancel := context.WithTimeout(context.Background(), 11*time.Second)
+
+    ctx, cancel := context.WithTimeout(context.Background(), 11*time.Second)//      Контекст с таймаутом на завершение (11 секунд > WriteTimeout)
     defer cancel()
 
-// 13. Останавливаем сервер
+//  Останавливаем сервер по сигналу от контекста
     if err := srv.Shutdown(ctx); err != nil {
        logger.Error("Server forced to shutdown: %v", err)
 	   os.Exit(1)
     }
 
    logger.Info("Server exited properly")
+   return nil
 }
 
+// Реализуем логику для rolling back
+func shouldRollback() bool {
+    return len(os.Args) > 1 && os.Args[1] == "-down"
 }
+func rollbackMigrations() error {
+    if err := database.RollbackMigrations(); err != nil {
+        return fmt.Errorf("rollback failed: %w", err)
+    }
+    logger.Info("Migration rolled back")
+    return nil
+}
+func applyMigrations() error {
+    if err := database.RunMigrations(); err != nil {
+        logger.Warn("Migrations warning (maybe already applied): %v", err)
+    }
+    return nil
+}
+    
